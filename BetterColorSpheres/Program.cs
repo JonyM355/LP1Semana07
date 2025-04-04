eea0786bd1caefec30010c557912ad749d9d4525
JonyM355 <joaoricardoam05@gmail.com>
2025-04-04 15:11:01 +0100

    Add class Program

---
+++ BetterColorSpheres/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace BetterColorSpheres
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //create colors
+            Color red = new Color(255,0,0, 50);
+            Color green = new Color(255,0,0);
+            Color blue = new Color(255,0,0);
+            Color yellow = new Color(0,255,255, 200);
+            Color grey = new Color(180,230,255);
+            grey.GetGrey();
+
+            //create spheres
+            Sphere s1 = new Sphere(red, 10);
+            Sphere s2 = new Sphere(green, 5);
+            Sphere s3 = new Sphere(blue, 3);
+            Sphere s4 = new Sphere(yellow, 50);
+            Sphere s5 = new Sphere(grey, 197);
+
+            //Sphere methods
+            s1.Pop();
+            s1.Throw();
+            Console.WriteLine($"Esfera 1:");
+            Console.WriteLine($"Color: {s1.GetColor()}");
+            Console.WriteLine($"Times throwned: {s1.GetTimesThrown()}");
+            Console.WriteLine($"Popped: {s1.IsPopped()}\n");
+
+
+            s2.Throw();
+            s2.Throw();
+            s2.Throw();
+            Console.WriteLine($"Esfera 2:");
+            Console.WriteLine($"Color: {s2.GetColor()}");
+            Console.WriteLine($"Times throwned: {s2.GetTimesThrown()}");
+            Console.WriteLine($"Popped: {s2.IsPopped()}\n");
+
+
+            s3.Throw();
+            s3.Pop();
+            s3.Throw();
+            Console.WriteLine($"Esfera 3:");
+            Console.WriteLine($"Color: {s3.GetColor()}");
+            Console.WriteLine($"Times throwned: {s3.GetTimesThrown()}");
+            Console.WriteLine($"Popped: {s3.IsPopped()}\n");
+
+            s4.Pop();
+            s4.Throw();
+            s4.Throw();
+            Console.WriteLine($"Esfera 4:");
+            Console.WriteLine($"Color: {s4.GetColor()}");
+            Console.WriteLine($"Times throwned: {s4.GetTimesThrown()}");
+            Console.WriteLine($"Popped: {s4.IsPopped()}\n");
+
+            s5.Throw();
+            s5.Throw();
+            s5.Throw();
+            s5.Throw();
+            s5.Throw();
+            Console.WriteLine($"Esfera 5:");
+            Console.WriteLine($"Color: {s5.GetColor().GetGrey()}");
+            Console.WriteLine($"Times throwned: {s5.GetTimesThrown()}");
+            Console.WriteLine($"Popped: {s5.IsPopped()}\n");
+        }
+    }
+}
